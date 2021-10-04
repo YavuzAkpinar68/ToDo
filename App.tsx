75b0de1a978dc363da4510dfd7cb2f6364bead75
yavuz <yavuz.akpinar.68@gmail.com>
2021-10-05 00:26:42 +0300

    ToDo uygulamsı tamamlandı

--- App.tsx
+++ App.tsx
@@ -1,21 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, FlatList, Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import styles from './style';
+
+
 
 export default function App() {
+  
+  const [todo, setToDo] = useState<string>("")
+  const [todos, setToDos] = useState<string[]>([])
+  const addToDo = () => {
+    setToDos([todo, ...todos])
+    setToDo("")
+    Keyboard.dismiss()
+  }
+  
+
+  const removeToDo = (index: number) => {
+    const newToDos = [...todos.slice(0, index), ...todos.slice(index + 1, todos.length)]
+    setToDos(newToDos)
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.outerView_container}>
+      <View style={styles.titleView_container}>
+        <Text style={styles.title_container}>Yapılacaklar</Text>
+        <Text style={styles.counter_container}>{todos.length}</Text>
+      </View>
+      <View style={styles.flatlistView_container}>
+        <FlatList
+          style={styles.flatlist_container}
+          data={todos}
+          renderItem={({item, index}) => 
+            <TouchableOpacity 
+              onPress={() => removeToDo(index)}
+              style={styles.flatlistinner_container}>
+              <Text 
+                style={styles.flatlistinnerText_container}>{item}
+              </Text>
+            </TouchableOpacity>}>
+         </FlatList>
+      </View>
+      <View style={styles.viewToucIndex_container} >
+        <View style={styles.textInput_container}> 
+          <TextInput
+            placeholder="Yapılacaklar..."
+            value={todo}
+            onSubmitEditing={addToDo}
+            onChangeText={setToDo}>
+          </TextInput>
+        </View>   
+       
+        <TouchableOpacity
+          style={styles.touchable_container}
+          onPress={addToDo}>
+            <Text style={{color:"silver"}}>Ekle...</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ style.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  outerView_container:{
+    justifyContent:"flex-end",
+    flex:1,
+    backgroundColor:"cadetblue"
+  },
+  textInputView_container:{
+    backgroundColor:"red",
+    margin:10,
+    borderRadius:15,
+    flex:2
+  },
+  textInput_container:{
+    backgroundColor:"gray",
+    margin:10,
+    borderBottomWidth:1,
+    borderColor:"silver"
+  },
+  
+  titleView_container:{
+    backgroundColor:"steelblue",
+    flexDirection:"row",
+    flex:1,
+    paddingTop:30,
+    paddingLeft:10,
+    borderBottomWidth:1,
+    borderColor:"silver"
+  },
+  flatlistView_container:{
+    backgroundColor:"steelblue",
+    margin:10,
+    borderRadius:15,
+    flex:9
+  },
+  touchable_container:{
+    paddingVertical:15,
+    backgroundColor:"lightslategrey",
+    paddingHorizontal:10,
+    borderRadius:12,
+    justifyContent:"center",
+    alignItems:"center",
+    margin:10
+  },
+  title_container:{
+    fontSize:30,
+    flex:5,
+    color:"yellow"
+  },
+  counter_container:{
+    fontSize:30,
+    flex:1,
+    color:"yellow"
+  },
+  flatlist_container:{
+
+  },
+  flatlistinner_container:{
+    margin:5,
+    borderRadius:10,
+    borderWidth:1,
+    backgroundColor:"cadetblue"
+  },
+  flatlistinnerText_container:{
+    paddingLeft:10,
+    fontSize:20,
+    color:"cornsilk"
+  },
+  viewToucIndex_container:{
+    backgroundColor:"gray",
+    margin:10,
+    borderRadius:15
+  }
+})
+
+export default styles
